495974e433ad33151c1d352bc4846e767bb0e411
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-20 20:38:03 -0300

    Change todo element to li

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,13 +2,13 @@ import cross from "../assets/icons/icon-cross.svg";
 
 const Todo = ({ text }) => {
   return (
-    <div className="todo">
+    <li className="todo">
       <button className="todo__btn"></button>
       <p className="todo__text">{text}</p>
       <button className="todo__close">
         <img className="todo__icon" src={cross} alt="Close" />
       </button>
-    </div>
+    </li>
   );
 };
 

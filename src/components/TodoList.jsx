98f4b56c9cfe4eadbc99ec5600819e7213d21774
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-20 20:25:47 -0300

    Add todo placeholders

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -6,8 +6,12 @@ const TodoList = () => {
       className="todo-list round"
       style={{ margin: "-27px 25px 0", overflow: "hidden" }}
     >
-      <Todo />
-      <Todo />
+      <Todo text="Complete online JavaScript course" />
+      <Todo text="Jog around the park 3x" />
+      <Todo text="10 minutes meditation" />
+      <Todo text="Read for 1 hour" />
+      <Todo text="Pickup groceries" />
+      <Todo text="Complete Todo App on Frontend Mentor" />
     </div>
   );
 };

509381bc3d674314f239416ecd3876cf3c56f91f
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-20 17:15:06 -0300

    Extract todo to component

---
+++ src/components/Todo.jsx
@@ -0,0 +1,14 @@
+const Todo = () => {
+  return (
+    <div className="todo">
+      <button className="todo__btn"></button>
+      <input
+        className="todo__entry"
+        type="text"
+        placeholder="Create a new todo..."
+      />
+    </div>
+  );
+};
+
+export default Todo;

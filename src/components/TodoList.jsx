b3dd548b383d65b2b97a32e880da600bdba3da5c
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-20 20:37:40 -0300

    Wrap todos around ul

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -6,12 +6,14 @@ const TodoList = () => {
       className="todo-list round"
       style={{ margin: "-27px 25px 0", overflow: "hidden" }}
     >
-      <Todo text="Complete online JavaScript course" />
-      <Todo text="Jog around the park 3x" />
-      <Todo text="10 minutes meditation" />
-      <Todo text="Read for 1 hour" />
-      <Todo text="Pickup groceries" />
-      <Todo text="Complete Todo App on Frontend Mentor" />
+      <ul>
+        <Todo text="Complete online JavaScript course" />
+        <Todo text="Jog around the park 3x" />
+        <Todo text="10 minutes meditation" />
+        <Todo text="Read for 1 hour" />
+        <Todo text="Pickup groceries" />
+        <Todo text="Complete Todo App on Frontend Mentor" />
+      </ul>
     </div>
   );
 };

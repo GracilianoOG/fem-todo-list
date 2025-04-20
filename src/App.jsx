0d55836a1fec58a0f6f8c6a5ee3429fa789e2143
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-20 16:03:52 -0300

    Extract header to component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,8 @@
 import "./App.css";
-import moon from "./assets/icons/icon-moon.svg";
+import Header from "./components/Header";
 
 const App = () => {
-  return (
-    <header className="header">
-      <div className="header__content">
-        <h1 className="header__title">Todo</h1>
-        <button className="theme-switcher">
-          <img className="theme-switcher__icon" src={moon} alt="" />
-        </button>
-      </div>
-      <div className="todo">
-        <button className="todo__btn"></button>
-        <input
-          className="todo__entry"
-          type="text"
-          placeholder="Create a new todo..."
-        />
-      </div>
-    </header>
-  );
+  return <Header />;
 };
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import moon from "../assets/icons/icon-moon.svg";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__content">
+        <h1 className="header__title">Todo</h1>
+        <button className="theme-switcher">
+          <img className="theme-switcher__icon" src={moon} alt="" />
+        </button>
+      </div>
+      <div className="todo">
+        <button className="todo__btn"></button>
+        <input
+          className="todo__entry"
+          type="text"
+          placeholder="Create a new todo..."
+        />
+      </div>
+    </header>
+  );
+};
+
+export default Header;

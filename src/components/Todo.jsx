af188527dabfe1dfd16a7a7dd1073ace28b4c337
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-22 19:16:14 -0300

    Add todo marking functionality

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import cross from "../assets/icons/icon-cross.svg";
 import "./Todo.css";
 
 const Todo = ({ text }) => {
+  const [isCompleted, setIsCompleted] = useState(false);
+
+  const handleMarkClick = () => {
+    setIsCompleted(prevSelected => !prevSelected);
+  };
+
   return (
     <li className="todo box">
-      <button className="todo__btn"></button>
-      <p className="todo__text">{text}</p>
+      <button
+        className={`todo__btn ${!isCompleted ? "" : "todo__btn--selected"}`}
+        onClick={handleMarkClick}
+      ></button>
+      <p
+        className={`todo__text ${!isCompleted ? "" : "todo__text--completed"}`}
+      >
+        {text}
+      </p>
       <button className="todo__close">
         <img className="todo__icon" src={cross} alt="Close" />
       </button>

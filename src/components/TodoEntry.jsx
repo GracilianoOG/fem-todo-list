df3fd822c74299f61c0ae305cb1d742cfd374d8b
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-23 18:51:12 -0300

    Add todo from input

--- src/components/TodoEntry.jsx
+++ src/components/TodoEntry.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import "./TodoEntry.css";
 
-const TodoEntry = () => {
+const TodoEntry = ({ addTodo }) => {
+  const [task, setTask] = useState("");
+
   return (
     <div className="todo-entry box round">
-      <button className="todo__btn"></button>
+      <button className="todo__btn" onClick={() => addTodo(task)}></button>
       <input
         className="todo-entry__input"
         type="text"
         placeholder="Create a new todo..."
+        value={task}
+        onChange={e => setTask(e.target.value)}
       />
     </div>
   );

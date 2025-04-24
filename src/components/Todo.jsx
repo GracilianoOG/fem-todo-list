4e94d85b8ce72320d35f8934c12adf7206bcd38d
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-24 10:15:48 -0300

    Change prop names to avoid confusion

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,23 +3,25 @@ import cross from "../assets/icons/icon-cross.svg";
 import "./Todo.css";
 import TodoContext from "../context/TodoContext";
 
-const Todo = ({ id, task, isCompleted: status }) => {
-  const [isCompleted, setIsCompleted] = useState(status);
+const Todo = ({ id, task, isCompleted }) => {
+  const [isTodoCompleted, setIsTodoCompleted] = useState(isCompleted);
   const { deleteTodo, completeTodo } = useContext(TodoContext);
 
   const handleMarkClick = () => {
-    setIsCompleted(prevSelected => !prevSelected);
+    setIsTodoCompleted(prevSelected => !prevSelected);
     completeTodo(id);
   };
 
   return (
     <li className="todo box">
       <button
-        className={`todo__btn ${!isCompleted ? "" : "todo__btn--selected"}`}
+        className={`todo__btn ${!isTodoCompleted ? "" : "todo__btn--selected"}`}
         onClick={handleMarkClick}
       ></button>
       <p
-        className={`todo__text ${!isCompleted ? "" : "todo__text--completed"}`}
+        className={`todo__text ${
+          !isTodoCompleted ? "" : "todo__text--completed"
+        }`}
       >
         {task}
       </p>

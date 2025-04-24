e1ce31391ed7f5cd17313928f3edbb20e553e525
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-24 12:03:56 -0300

    Change style to selected based on filter

--- src/components/TodoPanel.jsx
+++ src/components/TodoPanel.jsx
@@ -3,18 +3,30 @@ import "./TodoPanel.css";
 import TodoContext from "../context/TodoContext";
 
 const TodoPanel = () => {
-  const { setFilter } = useContext(TodoContext);
+  const { filter, setFilter } = useContext(TodoContext);
 
   return (
     <div className="todo-panel box round">
-      <button className="todo-panel__btn" onClick={() => setFilter("ALL")}>
+      <button
+        className={`todo-panel__btn ${
+          filter === "ALL" && "todo-panel__btn--selected"
+        }`}
+        onClick={() => setFilter("ALL")}
+      >
         All
       </button>
-      <button className="todo-panel__btn" onClick={() => setFilter("ACTIVE")}>
+      <button
+        className={`todo-panel__btn ${
+          filter === "ACTIVE" && "todo-panel__btn--selected"
+        }`}
+        onClick={() => setFilter("ACTIVE")}
+      >
         Active
       </button>
       <button
-        className="todo-panel__btn"
+        className={`todo-panel__btn ${
+          filter === "COMPLETED" && "todo-panel__btn--selected"
+        }`}
         onClick={() => setFilter("COMPLETED")}
       >
         Completed

dbd4945b72c37bb9e466ad75eea3f38b83bf3402
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-24 19:02:34 -0300

    Remove unused CSS imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./App.css";
 import Header from "./components/Header";
 import TodoList from "./components/TodoList";
 import TodoContext from "./context/TodoContext";

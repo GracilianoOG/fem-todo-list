119342d793de26a46d3f22c96f6607e3eac79e10
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-24 11:55:44 -0300

    Change filter on click

--- src/components/TodoPanel.jsx
+++ src/components/TodoPanel.jsx
@@ -1,11 +1,24 @@
+import { useContext } from "react";
 import "./TodoPanel.css";
+import TodoContext from "../context/TodoContext";
 
 const TodoPanel = () => {
+  const { setFilter } = useContext(TodoContext);
+
   return (
     <div className="todo-panel box round">
-      <button className="todo-panel__btn">All</button>
-      <button className="todo-panel__btn">Active</button>
-      <button className="todo-panel__btn">Completed</button>
+      <button className="todo-panel__btn" onClick={() => setFilter("ALL")}>
+        All
+      </button>
+      <button className="todo-panel__btn" onClick={() => setFilter("ACTIVE")}>
+        Active
+      </button>
+      <button
+        className="todo-panel__btn"
+        onClick={() => setFilter("COMPLETED")}
+      >
+        Completed
+      </button>
     </div>
   );
 };

54c337bcd08b5aad29df49d199ea34a9e545c6fd
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-26 11:16:35 -0300

    Add component to display if no todo available

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,21 +1,29 @@
 import { useContext } from "react";
 import Todo from "./Todo";
 import TodoContext from "../context/TodoContext";
+import { NoTodo } from "./styles/NoTodoStyled";
 
 const Todos = () => {
   const { getTodos, filter } = useContext(TodoContext);
+  const todos = getTodos(filter);
 
   return (
-    <ul>
-      {getTodos(filter).map((todo, index) => (
-        <Todo
-          id={todo.id}
-          key={index}
-          task={todo.task}
-          isCompleted={todo.isCompleted}
-        />
-      ))}
-    </ul>
+    <>
+      {todos.length > 0 ? (
+        <ul>
+          {todos.map((todo, index) => (
+            <Todo
+              id={todo.id}
+              key={index}
+              task={todo.task}
+              isCompleted={todo.isCompleted}
+            />
+          ))}
+        </ul>
+      ) : (
+        <NoTodo className="box">No tasks yet, add your first todo!</NoTodo>
+      )}
+    </>
   );
 };
 

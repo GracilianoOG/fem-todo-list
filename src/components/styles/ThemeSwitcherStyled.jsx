32635be65e73cd4d7b539008b6643024fd3865a6
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-25 22:35:31 -0300

    Add styled component for theme switcher

---
+++ src/components/styles/ThemeSwitcherStyled.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ThemeSwitcherStyled = styled.button`
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+export const ThemeSwitcherIconStyled = styled.img`
+  width: 20px;
+`;

b4f1bbe17673c9d86c4841edcacf0410ebd4cd34
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-26 12:02:32 -0300

    Adjust theme switcher size to desktop

--- src/components/styles/ThemeSwitcherStyled.jsx
+++ src/components/styles/ThemeSwitcherStyled.jsx
@@ -1,11 +1,18 @@
 import styled from "styled-components";
 
 export const ThemeSwitcherStyled = styled.button`
+  --btn-size: 20px;
   border: none;
   background-color: transparent;
   cursor: pointer;
+  height: var(--btn-size);
+  width: var(--btn-size);
+
+  @media screen and (min-width: 768px) {
+    --btn-size: auto;
+  }
 `;
 
 export const ThemeSwitcherIconStyled = styled.img`
-  width: 20px;
+  width: 100%;
 `;

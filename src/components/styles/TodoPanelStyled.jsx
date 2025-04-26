ed155ebaaaf89c137441fdbdd430d978b9d68e8e
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-26 11:25:04 -0300

    Fix missing background color

--- src/components/styles/TodoPanelStyled.jsx
+++ src/components/styles/TodoPanelStyled.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const TodoPanelStyled = styled.div`
   box-shadow: 0 0 20px 0px rgba(0, 0, 0, 0.1);
+  background-color: ${({ theme }) => theme.colors.listBg};
   display: flex;
   align-items: center;
   justify-content: center;

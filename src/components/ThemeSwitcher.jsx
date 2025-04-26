55e459e256d2dc62a329bc2193184a337c821ccb
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-25 22:35:58 -0300

    Replace CSS classes with styled components

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -2,14 +2,18 @@ import { useContext } from "react";
 import DarkModeContext from "../context/DarkModeContext";
 import moon from "../assets/icons/icon-moon.svg";
 import sun from "../assets/icons/icon-sun.svg";
+import {
+  ThemeSwitcherIconStyled,
+  ThemeSwitcherStyled,
+} from "./styles/ThemeSwitcherStyled";
 
 const ThemeSwitcher = () => {
   const { isDark, setIsDark } = useContext(DarkModeContext);
 
   return (
-    <button className="theme-switcher" onClick={() => setIsDark(!isDark)}>
-      <img className="theme-switcher__icon" src={!isDark ? moon : sun} alt="" />
-    </button>
+    <ThemeSwitcherStyled onClick={() => setIsDark(!isDark)}>
+      <ThemeSwitcherIconStyled src={!isDark ? moon : sun} alt="" />
+    </ThemeSwitcherStyled>
   );
 };
 

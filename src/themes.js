45fca582e51162ba3a0a27a5f9807dae209dd7e0
Gabriel <gabriel.gracilianomb@gmail.com>
2025-04-25 22:05:59 -0300

    Add dark theme
    A few new properties were added to both themes to match other colors.

--- src/themes.js
+++ src/themes.js
@@ -1,16 +1,20 @@
 import bgImageMobileLight from "./assets/images/bg-mobile-light.jpg";
 import bgImageDesktopLight from "./assets/images/bg-desktop-light.jpg";
+import bgImageMobileDark from "./assets/images/bg-mobile-dark.jpg";
+import bgImageDesktopDark from "./assets/images/bg-desktop-dark.jpg";
 
 const lightTheme = {
   colors: {
     buttonBorder: "var(--color-very-light-grayish-blue)",
     buttonGradient: "var(--color-check-background)",
+    buttonBgHover: "linear-gradient(#fff, #fff)",
     bodyBgColor: "var(--color-very-light-gray)",
     bodyColor: "var(--color-very-dark-grayish-blue)",
     filterBtn: "var(--color-dark-grayish-blue)",
     filterBtnSelected: "var(--color-bright-blue)",
     headerTitle: "var(--color-very-light-gray)",
     input: "var(--color-very-dark-grayish-blue)",
+    inputBg: "#fff",
     inputPlaceholder: "var(--color-dark-grayish-blue)",
     listText: "var(--color-dark-grayish-blue)",
     listBg: "#fff",
@@ -23,4 +27,29 @@ const lightTheme = {
   },
 };
 
-export { lightTheme };
+const darkTheme = {
+  colors: {
+    buttonBorder: "var(--color-very-dark-grayish-blue-deep)",
+    buttonGradient: "var(--color-check-background)",
+    buttonBgHover:
+      "linear-gradient(var(--color-very-dark-desaturated-blue), var(--color-very-dark-desaturated-blue))",
+    bodyBgColor: "var(--color-very-dark-blue)",
+    bodyColor: "var(--color-light-grayish-blue-dark)",
+    filterBtn: "var(--color-very-dark-grayish-blue-dark)",
+    filterBtnSelected: "var(--color-bright-blue)",
+    headerTitle: "var(--color-very-light-gray)",
+    input: "var(--color-dark-grayish-blue)",
+    inputBg: "var(--color-very-dark-desaturated-blue)",
+    inputPlaceholder: "var(--color-dark-grayish-blue)",
+    listText: "var(--color-dark-grayish-blue)",
+    listBg: "var(--color-very-dark-desaturated-blue)",
+    todoBorder: "var(--color-very-dark-grayish-blue-deep)",
+    todoCrossed: "var(--color-very-dark-grayish-blue-dark)",
+  },
+  images: {
+    headerMobile: bgImageMobileDark,
+    headerDesktop: bgImageDesktopDark,
+  },
+};
+
+export { lightTheme, darkTheme };

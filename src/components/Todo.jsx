98cc1d81fa87f8c87def78396eb9c814825b97e2
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-03 18:41:17 -0300

    Handle drag operations

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -9,14 +9,54 @@ import {
   TodoTextStyled,
 } from "./styles/TodoStyled";
 
-const Todo = ({ id, task, isCompleted }) => {
+const Todo = ({ id, task, isCompleted, position, dragInfo, setDragInfo }) => {
   const { deleteTodo, completeTodo } = useContext(TodoContext);
 
   const handleCompleteClick = () => completeTodo(id);
   const handleDeleteClick = () => deleteTodo(id);
 
+  const handleDragStart = e => {
+    setDragInfo({
+      ...dragInfo,
+      from: e.target.dataset.position,
+    });
+    console.log("dragging from " + dragInfo.from);
+  };
+
+  const handleDragOver = e => {
+    e.preventDefault();
+    setDragInfo({
+      ...dragInfo,
+      to: e.currentTarget.dataset.position,
+    });
+    console.log("Dragging to " + dragInfo.to);
+  };
+
+  const handleDragLeave = () => {
+    console.log("Leave " + dragInfo.to);
+    setDragInfo({
+      ...dragInfo,
+      to: null,
+    });
+  };
+
+  const handleDrop = () => {
+    console.log(`Dropped ${dragInfo.from} on ${dragInfo.to}`);
+    setDragInfo({
+      ...dragInfo,
+      to: null,
+    });
+  };
+
   return (
-    <TodoStyled>
+    <TodoStyled
+      data-position={position}
+      draggable={true}
+      onDragStart={handleDragStart}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      onDrop={handleDrop}
+    >
       <ButtonStyled
         $selected={isCompleted}
         onClick={handleCompleteClick}

1de3df3749beaf23be61c30ec7975c4dd2673bc6
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-06 11:24:06 -0300

    Add scale transition on hover

--- src/components/styles/ThemeSwitcherStyled.jsx
+++ src/components/styles/ThemeSwitcherStyled.jsx
@@ -3,7 +3,12 @@ import styled from "styled-components";
 export const ThemeSwitcherStyled = styled.button`
   --btn-size: 1.25rem;
   height: var(--btn-size);
+  transition: transform 0.25s;
   width: var(--btn-size);
+
+  &:hover {
+    transform: scale(1.15);
+  }
 
   @media screen and (min-width: 48rem) {
     --btn-size: auto;

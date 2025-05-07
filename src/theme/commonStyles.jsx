e6d5a1f71a88b7c090fec7c1c73fa11418de49c4
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-07 11:22:00 -0300

    Change px unit to rem

--- src/theme/commonStyles.jsx
+++ src/theme/commonStyles.jsx
@@ -3,10 +3,10 @@ import { css } from "styled-components";
 export const box = css`
   align-items: center;
   display: flex;
-  padding: 16px 20px;
+  padding: 1rem 1.25rem;
 
-  @media screen and (min-width: 768px) {
-    padding: 20px 24px;
+  @media screen and (min-width: 48rem) {
+    padding: 1.25rem 1.5rem;
   }
 `;
 

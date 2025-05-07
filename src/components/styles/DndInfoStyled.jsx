0b0dbd313772377de7741bef3072f5093a5b9f56
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-07 12:47:49 -0300

    Add margin bottom

--- src/components/styles/DndInfoStyled.jsx
+++ src/components/styles/DndInfoStyled.jsx
@@ -3,5 +3,6 @@ import styled from "styled-components";
 export const DndInfoStyled = styled.p`
   color: ${({ theme }) => theme.colors.listText};
   font-size: 0.856rem;
+  margin-bottom: 5rem;
   text-align: center;
 `;

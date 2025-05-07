6b91782ee95f9e25ad4b4f74ac2fb8476b0e4c12
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-07 09:52:23 -0300

    Add info about drag and drop

---
+++ src/components/styles/DndInfoStyled.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const DndInfoStyled = styled.p`
+  color: ${({ theme }) => theme.colors.listText};
+  font-size: 0.856rem;
+  text-align: center;
+`;

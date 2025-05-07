e72ec9ba03c5bb35c6c843dd7db503e19fb0f2d0
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-07 12:40:53 -0300

    Create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import { FooterStyled } from "./styles/FooterStyled";
+
+const Footer = () => {
+  return (
+    <FooterStyled>
+      <p>
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://github.com/GracilianoOG">Gabriel</a>.
+      </p>
+    </FooterStyled>
+  );
+};
+
+export default Footer;

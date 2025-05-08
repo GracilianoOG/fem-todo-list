c09b2fd1a697ed0029b161f2ec4ec0cbef0c460c
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-08 16:37:39 -0300

    Use filter context

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,14 +2,20 @@ import { TodoListStyled } from "./styles/TodoListStyled";
 import Todos from "./Todos";
 import TodoListFooter from "./TodoListFooter";
 import TodoEntry from "./TodoEntry";
+import FilterContext from "../context/FilterContext";
+import { useState } from "react";
 
 const TodoList = () => {
+  const [filter, setFilter] = useState("ALL");
+
   return (
     <>
       <TodoEntry />
       <TodoListStyled>
-        <Todos />
-        <TodoListFooter />
+        <FilterContext.Provider value={{ filter, setFilter }}>
+          <Todos />
+          <TodoListFooter />
+        </FilterContext.Provider>
       </TodoListStyled>
     </>
   );

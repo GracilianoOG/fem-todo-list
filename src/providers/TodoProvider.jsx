261488a760db1878cf41edb558ddb4088c4494e4
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-08 20:35:26 -0300

    Create todo provider

---
+++ src/providers/TodoProvider.jsx
@@ -0,0 +1,11 @@
+import { useTodos } from "./hooks/useTodos";
+
+const TodoProvider = ({ children }) => {
+  return (
+    <TodoContext.Provider value={{ ...useTodos() }}>
+      {children}
+    </TodoContext.Provider>
+  );
+};
+
+export default TodoProvider;

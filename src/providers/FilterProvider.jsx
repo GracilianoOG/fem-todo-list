a814e715c5347d17151db5333bf0322dd40f08e0
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-08 21:22:25 -0300

    Cache filter value

--- src/providers/FilterProvider.jsx
+++ src/providers/FilterProvider.jsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import FilterContext from "../context/FilterContext";
 
 const FilterProvider = ({ children }) => {
   const [filter, setFilter] = useState("ALL");
+  const value = useMemo(() => {
+    return { filter, setFilter };
+  }, [filter]);
 
   return (
-    <FilterContext.Provider value={{ filter, setFilter }}>
-      {children}
-    </FilterContext.Provider>
+    <FilterContext.Provider value={value}>{children}</FilterContext.Provider>
   );
 };
 
